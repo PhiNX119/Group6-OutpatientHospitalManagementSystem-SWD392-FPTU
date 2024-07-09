77ef03b0bb21045f9819fbb6f3a1e46d58cb97da
Do Hoang Cong Minh <dhcongminh@gmail.com>
2024-07-09 17:37:31 +0700

    add methods

--- src/main/java/com/swd392/group6/outpatientmanagementsystem/service/MedicalExaminationHistoryService.java
+++ src/main/java/com/swd392/group6/outpatientmanagementsystem/service/MedicalExaminationHistoryService.java
@@ -1,6 +1,9 @@
 package com.swd392.group6.outpatientmanagementsystem.service;
 
+import com.swd392.group6.outpatientmanagementsystem.model.dto.MedicalExaminationHistoryDto;
 import com.swd392.group6.outpatientmanagementsystem.model.entity.MedicalExaminationHistory;
+import com.swd392.group6.outpatientmanagementsystem.model.entity.MedicalRecord;
+import com.swd392.group6.outpatientmanagementsystem.model.entity.PatientInfo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,4 +11,8 @@ import java.util.List;
 @Service
 public interface MedicalExaminationHistoryService {
     List<MedicalExaminationHistory> GetAll();
+    List<PatientInfo> GetAllPatients();
+    boolean AddNewMedicalExaminationHistory(MedicalExaminationHistoryDto mehDto);
+
+    List<MedicalRecord> GetAllMedicalRecords();
 }
