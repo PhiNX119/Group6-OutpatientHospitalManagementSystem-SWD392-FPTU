89dfe54e2a8cfa2a29cf436f49fbb05a9f586880
Do Hoang Cong Minh <dhcongminh@gmail.com>
2024-07-09 17:37:10 +0700

    get and post: /add

--- src/main/java/com/swd392/group6/outpatientmanagementsystem/controller/MedicalExaminationHistoryController.java
+++ src/main/java/com/swd392/group6/outpatientmanagementsystem/controller/MedicalExaminationHistoryController.java
@@ -2,18 +2,25 @@ package com.swd392.group6.outpatientmanagementsystem.controller;
 
 
 import com.swd392.group6.outpatientmanagementsystem.model.dto.MedicalExaminationHistoryDto;
+import com.swd392.group6.outpatientmanagementsystem.model.dto.MedicineDto;
 import com.swd392.group6.outpatientmanagementsystem.model.entity.MedicalExaminationHistory;
+import com.swd392.group6.outpatientmanagementsystem.repository.PatientInfoRepository;
 import com.swd392.group6.outpatientmanagementsystem.service.MedicalExaminationHistoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/medical-examination-history")
 public class MedicalExaminationHistoryController {
     private final MedicalExaminationHistoryService service;
 
@@ -32,5 +39,31 @@ public class MedicalExaminationHistoryController {
         }
         model.addAttribute("examinationList", examinationDtoList);
         return "medical-examination-history/list";
+    }
+
+
+    @GetMapping("/add")
+    public String addNewMedicalExaminationHistory(Model model) {
+        MedicalExaminationHistoryDto medicalExaminationHistoryDto = new MedicalExaminationHistoryDto();
+        model.addAttribute("medicalExaminationHistoryDto", medicalExaminationHistoryDto);
+        model.addAttribute("patientList", service.GetAllPatients());
+        model.addAttribute("medicalRecordList", service.GetAllMedicalRecords());
+        return "medical-examination-history/add";
+    }
+
+
+    @PostMapping("/add")
+    public String addNewCar(@Valid @ModelAttribute("medicalExaminationHistoryDto") MedicalExaminationHistoryDto medicalExaminationHistoryDto,
+                            BindingResult result,
+                            Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("medicalExaminationHistoryDto", medicalExaminationHistoryDto);
+            model.addAttribute("patientList", service.GetAllPatients());
+            model.addAttribute("medicalRecordList", service.GetAllMedicalRecords());
+            return "medical-examination-history/add";
+        } else {
+
+            return "redirect:/medical-examination-history/list?addSuccess";
+        }
     }
 }
